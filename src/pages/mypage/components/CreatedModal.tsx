37aa45d4f0692544a4de5eddb07fa568f9016e4b
taewok <taewok5163@naver.com>
2024-11-22 00:03:46 +0900

    feat: 만들기 Modal 컴포넌트 생성

---
+++ src/pages/mypage/components/CreatedModal.tsx
@@ -0,0 +1,30 @@
+const CreatedModal = () => {
+    return (
+        <div
+            style={{
+                boxShadow: '0px 0px 8px 0px rgba(0, 0, 0, 0.1)',
+            }}
+            className="z-50 rounded-[16px] border-[1px] w-[198px] absolute bottom-[-8px] right-[0%] transform  translate-y-full px-[8px] py-[8px] text-[12px] text-[#111111] bg-white rounded shadow-lg"
+        >
+            <p className="font-nomal px-[8px] py-[8px] text-[12px] text-[#111111]">
+                만들기
+            </p>
+            <p
+                className={
+                    'hover:bg-[#e9e9e9] flex justify-between items-center cursor-pointer rounded-[8px] font-semibold text-[16px] px-[8px] py-[8px]'
+                }
+            >
+                <span>핀</span>
+            </p>
+            <p
+                className={
+                    'hover:bg-[#e9e9e9] flex justify-between items-center cursor-pointer rounded-[8px] font-semibold text-[16px] px-[8px] py-[8px]'
+                }
+            >
+                <span>보드</span>
+            </p>
+        </div>
+    );
+};
+
+export default CreatedModal;

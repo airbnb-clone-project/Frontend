d304f395983ffaf59dfaf08acb60ac3dcb6ba0f8
taewok <taewok5163@naver.com>
2024-11-30 16:49:27 +0900

    feat: 브라우저 창과의 top거리가 0이면 modal close

--- src/pages/mypage/components/CreatedModal.tsx
+++ src/pages/mypage/components/CreatedModal.tsx
@@ -1,10 +1,23 @@
+import { useRef } from 'react';
 import ModalLayout from '../../../components/@Modal/ModalLayout';
+import { useIntersectionWithTopCheck } from '@/hooks/useIntersectionWithTopCheck';
+import useModalStore from '@/stores/useModalStore';
 
 const CreatedModal = () => {
+    const { toggleModal } = useModalStore();
+    const createdModalRef = useRef<HTMLDivElement>(null);
+
+    // Hook 사용
+    useIntersectionWithTopCheck({
+        modalRef: createdModalRef,
+        threshold: 1.0,
+        modalName: 'filter',
+    });
     return (
         <>
             <ModalLayout modalName={'create'}></ModalLayout>
             <div
+                ref={createdModalRef}
                 style={{
                     boxShadow: '0px 0px 8px 0px rgba(0, 0, 0, 0.1)',
                 }}
@@ -21,6 +34,7 @@ const CreatedModal = () => {
                     <span>핀</span>
                 </p>
                 <p
+                    onClick={() => toggleModal('boardCreate')}
                     className={
                         'hover:bg-[#e9e9e9] flex justify-between items-center cursor-pointer rounded-[8px] font-semibold text-[16px] px-[8px] py-[8px]'
                     }

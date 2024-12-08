60d3c345bb5b4fa8475eda25b0e4031821c69328
taewok <taewok5163@naver.com>
2024-12-08 19:32:59 +0900

    feat: 위 방향 화살표 icon 추가

---
+++ src/components/icons/UpArrowIcon.tsx
@@ -0,0 +1,15 @@
+const UpArrowIcon = () => {
+    return (
+        <svg
+            aria-label="추가 옵션 화살표 아이콘"
+            height="12"
+            role="img"
+            viewBox="0 0 24 24"
+            width="12"
+        >
+            <path d="M.66 14.2a2.2 2.2 0 0 0 0 3.15c.88.87 2.3.87 3.18 0L12 9.29l8.16 8.06c.88.87 2.3.87 3.18 0a2.2 2.2 0 0 0 0-3.15L12 3z"></path>
+        </svg>
+    );
+};
+
+export default UpArrowIcon;

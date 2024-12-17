51485cec03e52ec8a7ab5c85af1f8c8d1ea855c1
bomlang <bomlang4211@gmail.com>
2024-12-18 04:01:34 +0900

    modify: 라우터 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from './pages/App';
-import PinDetail from './pages/@PinDetail/PinDetail';
+import PinDetail from './pages/pinDetail/PinDetail';
 import Mypage from './pages/mypage/Mypage';
 import Created from './pages/mypage/components/Created';
 import Saved from './pages/mypage/components/savedTab/Saved';

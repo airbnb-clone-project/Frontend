c3882688ae78e3cd9e91de876ac6b60e2041d67a
taewok <taewok5163@naver.com>
2024-12-17 23:49:44 +0900

    feat: ... 모양 icon 추가

---
+++ src/components/icons/ThreeDotIcon.tsx
@@ -0,0 +1,16 @@
+const ThreeDotIcon = () => {
+    return (
+        <svg
+            aria-hidden="true"
+            aria-label=""
+            height="16"
+            role="img"
+            viewBox="0 0 24 24"
+            width="16"
+        >
+            <path d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6M3 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6m18 0a3 3 0 1 0 0 6 3 3 0 0 0 0-6"></path>
+        </svg>
+    );
+};
+
+export default ThreeDotIcon;

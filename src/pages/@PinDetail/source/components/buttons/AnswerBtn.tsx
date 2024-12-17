d497d2ea03a162ab09f65a773ab1e1f76b4f5de4
bomlang <bomlang4211@gmail.com>
2024-12-18 04:07:04 +0900

    refactor: 폴더 구조 변경 및 import 경로 수정

--- src/pages/@PinDetail/source/components/buttons/AnswerBtn.tsx
+++
@@ -1,9 +0,0 @@
-const AnswerBtn = () => {
-    return (
-        <button aria-label="답변" aria-disabled={false} className="mx-2.5">
-            <span className="text-[#767676] text-sm font-normal">답변</span>
-        </button>
-    );
-};
-
-export default AnswerBtn;

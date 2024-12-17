72056a648828cef1a3ea5acef5ece985683ab2c9
taewok <taewok5163@naver.com>
2024-12-17 23:47:20 +0900

    feat: - icon 추가

---
+++ src/components/icons/MinusIcon.tsx
@@ -0,0 +1,17 @@
+const MinusIcon = () => {
+    return (
+        <svg
+            aria-hidden="true"
+            aria-label=""
+            height="8"
+            role="img"
+            viewBox="0 0 24 24"
+            width="8"
+            fill="white"
+        >
+            <path d="M24 12a2 2 0 0 1-2 2H2a2 2 0 1 1 0-4h20a2 2 0 0 1 2 2"></path>
+        </svg>
+    );
+};
+
+export default MinusIcon;

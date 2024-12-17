04a0f53f3fa9e8c987e0132dbab86ae98f3f3445
bomlang <bomlang4211@gmail.com>
2024-12-18 04:16:59 +0900

    feat: 상세페이지에 필요한 각 섹션 컴포넌트화

---
+++ src/pages/pinDetail/source/components/PinDetailNav.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from 'react';
+
+import SaveBtn from './buttons/SaveBtn';
+import ShareBtn from './buttons/ShareBtn';
+import HeartBtn from './buttons/HeartBtn';
+import OptionsBtn from './buttons/OptionsBtn';
+import MyBoardBtn from './buttons/MyBoardBtn';
+
+// ref의 타입을 명확하게 지정해줍니다.
+const PinDetailNav = forwardRef<HTMLDivElement>((_, ref) => {
+    return (
+        <div ref={ref}>
+            <div className="pr-8 pl-5 pt-8 bg-white rounded-r-3xl z-30">
+                <div className="min-h-[60px] h-[60px] flex items-center justify-between">
+                    <div id="action-items" className="flex items-center">
+                        <HeartBtn />
+                        <ShareBtn />
+                        <OptionsBtn />
+                    </div>
+                    <div id="save-items" className="flex items-center gap-1">
+                        <MyBoardBtn />
+                        <SaveBtn />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+});
+
+export default PinDetailNav;

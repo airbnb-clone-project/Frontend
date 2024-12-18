ef76452d9e9ae22f753e884b6f4900852b67f388
taewok <taewok5163@naver.com>
2024-12-18 11:55:14 +0900

    feat: ref를 통해 textarea의 글자수가 넘칠 시 높이 증가

--- src/hooks/pinCreate/usePinForm.ts
+++ src/hooks/pinCreate/usePinForm.ts
@@ -1,12 +1,14 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 /**
  * @returns title: 핀 제목 상태
  * @returns explain: 핀 설명 상태
  * @returns link: 핀 링크 상태
+ * @returns textareaRef: textarea ref
  * @returns titleOnChange: 핀 제목을 변경하는 함수
  * @returns explainOnChange: 핀 설명을 변경하는 함수
  * @returns linkOnChange: 핀 링크를 변경하는 함수
+ * @returns handleResizeHeight: textarea태그에 들어가는 text 길이에 따른 height증가 함수
  */
 export const usePinForm = () => {
     const [title, setTitle] = useState<string>('');
@@ -14,15 +16,32 @@ export const usePinForm = () => {
     const [link, setLink] = useState<string>('');
 
     const titleOnChange = (text: string) => setTitle(text);
-    const explainOnChange = (text: string) => setExplain(text);
+
+    const explainOnChange = (text: string) => {
+        setExplain(text);
+        handleResizeHeight();
+    };
+
     const linkOnChange = (text: string) => setLink(text);
+
+    const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+    const handleResizeHeight = () => {
+        const currentTextarea = textareaRef.current;
+
+        if (currentTextarea) {
+            currentTextarea.style.height = 'auto'; //height 초기화
+            currentTextarea.style.height = currentTextarea.scrollHeight + 'px';
+        }
+    };
 
     return {
         title,
         explain,
         link,
+        textareaRef,
         titleOnChange,
         explainOnChange,
         linkOnChange,
+        handleResizeHeight,
     };
 };
--- src/pages/pinCreate/components/PinExplainInput.tsx
+++ src/pages/pinCreate/components/PinExplainInput.tsx
@@ -1,15 +1,23 @@
+import { LegacyRef } from 'react';
+
 interface PinExplainInputProps {
     value: string;
     onChangeFC: (text: string) => void;
+    textareaRef: LegacyRef<HTMLTextAreaElement> | undefined;
 }
-const PinExplainInput = ({ onChangeFC, value }: PinExplainInputProps) => {
+const PinExplainInput = ({
+    onChangeFC,
+    value,
+    textareaRef,
+}: PinExplainInputProps) => {
     return (
         <div className="">
             <p className="text-xs mb-2">설명</p>
             <textarea
+                ref={textareaRef}
                 value={value}
                 onChange={(v) => onChangeFC(v.target.value)}
-                className={`hover:border-[#a5a5a5] resize-none min-h-[96px] w-full border-[#cdcdcd] focus:outline-none focus:ring-4 focus:ring-[rgba(68, 132, 192, 0.5)] border-2 px-4 py-3 rounded-2xl focus:outline-[rgba(0, 132, 255, .5)] `} // 포커스 시 파란색 테두리
+                className={`max-h-[182px] hover:border-[#a5a5a5] resize-none min-h-[96px] w-full border-[#cdcdcd] focus:outline-none focus:ring-4 focus:ring-[rgba(68, 132, 192, 0.5)] border-2 px-4 py-3 rounded-2xl focus:outline-[rgba(0, 132, 255, .5)] `} // 포커스 시 파란색 테두리
                 placeholder="자세한 설명을 추가하세요."
             ></textarea>
         </div>

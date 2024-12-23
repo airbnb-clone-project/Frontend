9088408f118dea2b04be4270e31e2d84a89cc6bb
bomlang <bomlang4211@gmail.com>
2024-12-24 00:12:36 +0900

    modify: 메인페이지 수정

--- src/pages/main/components/imagebox/ImageBox.tsx
+++ src/pages/main/components/imagebox/ImageBox.tsx
@@ -3,49 +3,49 @@ import Overlay from './overlay/Overlay';
 import useModalStore from '../../../../stores/useModalStore';
 
 const ImageBox = ({
-    image,
-    itemWidth,
-    index,
+  image,
+  itemWidth,
+  index,
 }: {
-    image: string;
-    itemWidth: number;
-    index: number;
+  image: string;
+  itemWidth: number;
+  index: number;
 }) => {
-    const [hoverBox, setHoverBox] = useState(false);
-    const { id, isActiveButton } = useModalStore();
-    const handelHoverBox = (bol: boolean) => {
-        setHoverBox(bol);
-    };
+  const [hoverBox, setHoverBox] = useState(false);
+  const { id, isActiveButton } = useModalStore();
+  const handelHoverBox = (bol: boolean) => {
+    setHoverBox(bol);
+  };
 
-    const isActive = () => {
-        return (
-            isActiveButton['profile'] ||
-            isActiveButton['recommendPin'] ||
-            isActiveButton['share']
-        );
-    };
-
+  const isActive = () => {
     return (
-        <>
-            <div
-                className="relative grid-item flex justify-center items-center rounded-xl bg-white mb-4 cursor-pointer font-bold"
-                style={{ width: `${itemWidth}px` }}
-                onMouseEnter={() => handelHoverBox(true)}
-                onMouseLeave={() => handelHoverBox(false)}
-            >
-                {(hoverBox || (id === index && isActive())) && (
-                    <Overlay handelHoverBox={handelHoverBox} index={index} />
-                )}
-                <img
-                    src={image}
-                    alt="이미지"
-                    className={`w-full bg-[#F5F5F5] z-0 rounded-xl ${
-                        hoverBox ? 'brightness-[.70]' : ''
-                    }`}
-                />
-            </div>
-        </>
+      isActiveButton['profile'] ||
+      isActiveButton['recommendPin'] ||
+      isActiveButton['share']
     );
+  };
+
+  return (
+    <>
+      <div
+        className="relative grid-item flex justify-center items-center rounded-xl bg-white mb-4 cursor-pointer font-bold"
+        style={{ width: `${itemWidth}px` }}
+        onMouseEnter={() => handelHoverBox(true)}
+        onMouseLeave={() => handelHoverBox(false)}
+      >
+        {(hoverBox || (id === index && isActive())) && (
+          <Overlay handelHoverBox={handelHoverBox} index={index} />
+        )}
+        <img
+          src={image}
+          alt="이미지"
+          className={`w-full bg-[#F5F5F5] z-0 rounded-xl ${
+            hoverBox ? 'brightness-[.70]' : ''
+          }`}
+        />
+      </div>
+    </>
+  );
 };
 export default ImageBox;
 // 498 432

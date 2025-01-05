a1b0bdf2900cee4b180987304254301d33ab2c8a
taewok <taewok5163@naver.com>
2025-01-05 13:29:23 +0900

    feat: 사용자 정보 불러오기 api 코드

---
+++ src/services/getUserProfile.ts
@@ -0,0 +1,9 @@
+import { api } from '.';
+
+/**
+ * @param getUserProfile - 사용자 정보 불러오기 api
+ */
+export const getUserProfile = async () => {
+  const { data } = await api.get(`/api/auth/profile`);
+  return data.data;
+};

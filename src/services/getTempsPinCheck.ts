94fe16821cc59e25505de5e2a4f4f414f2b60d95
taewok <taewok5163@naver.com>
2025-01-15 14:21:37 +0900

    feat: null허용 타입 추가 & userName을 로컬스토리지에서 get

--- src/services/getTempsPinCheck.ts
+++ src/services/getTempsPinCheck.ts
@@ -3,22 +3,21 @@ import { api } from '.';
 export interface tempPin {
   boardNo: number | null;
   commentAllowed: boolean;
-  createdAt: string;
+  createdAt: string | null;
   description: string | null;
-  imageClassification: string;
-  imgUrl: string;
+  imageClassification: string | null;
+  imgUrl: string | null;
   link: string | null;
-  tempPinNo: string;
+  tempPinNo: string | null;
   title: string | null;
-  updatedAt: string;
+  updatedAt: string | null;
 }
 
 /**
  * @param getTempPinCheck - 사용자의 임시핀을 모두 조회 api
  */
-export const getTempsPinCheck = async (
-  userName: string
-): Promise<tempPin[]> => {
+export const getTempsPinCheck = async (): Promise<tempPin[]> => {
+  const userName = localStorage.getItem('userName');
   const { data } = await api.get(`/api/pins/pin/temps/${userName}/v1`);
   return data.data;
 };

b2e72d54525d822af699fda5246f4433a58ba113
taewok <taewok5163@naver.com>
2025-01-19 00:46:06 +0900

    feat: 임시핀 삭제 api test완료

---
+++ src/services/deleteTempPin.ts
@@ -0,0 +1,11 @@
+import { api } from '.';
+
+/**
+ * @param deleteTempPin - 임시핀 삭제 api
+ */
+export const deleteTempPin = async (tempPinIds: string[]) => {
+  const { data } = await api.delete(`/api/pins/pin/temp/v1`, {
+    data: { tempPinIds: tempPinIds },
+  });
+  return data.data;
+};

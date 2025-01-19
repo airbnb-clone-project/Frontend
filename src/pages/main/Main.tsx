a5451a4c17a4694ac5b135463a9858655061dc41
bomlang <bomlang4211@gmail.com>
2025-01-19 10:32:06 +0900

    feat: 리액트쿼리로 데이터패칭

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,23 +1,35 @@
-import { useEffect, useState } from 'react';
-import { pin } from '@/services/getPins';
-import ShowImages from './components/showimages/ShowImages';
+import { useQuery } from '@tanstack/react-query';
 
-const Main = () => {
-  const [pinData, setPinData] = useState<PinResponse<Pin[]> | null>(null);
+// import { pin } from '@/services/getPins';
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await pin.getPins();
-      setPinData(res);
-      console.log(res);
-    };
-    fetchData();
-  }, []);
+import ShowImages from './source/components/ShowImages';
+import { fetchPins } from './source/__mock__/getPins';
+
+const MainPage = () => {
+  const {
+    data: pinData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery<PinListResponse>({
+    queryKey: ['pins'],
+    // queryFn: () => pin.getPins(),
+    queryFn: () => fetchPins(),
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
-    <>
-      <ShowImages data={pinData} />
-    </>
+    <main>
+      <ShowImages pinData={pinData} />
+    </main>
   );
 };
-export default Main;
+
+export default MainPage;

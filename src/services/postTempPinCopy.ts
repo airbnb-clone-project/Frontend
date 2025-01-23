78bad10fd23ca4a10b5bf3838e41a5597cdc023a
taewok <taewok5163@naver.com>
2025-01-23 23:47:17 +0900

    feat: 임시핀 복제 api 코드 작성완료

---
+++ src/services/postTempPinCopy.ts
@@ -0,0 +1,11 @@
+import { api } from '.';
+
+/**
+ * @param postTempPinCopy - 임시핀 복제 api
+ */
+export const postTempPinCopy = async (tempPinNo: string) => {
+  const { data } = await api.post(`/api/pins/pin/temp/copy/v1`, {
+    copyTargetTempPinNo: tempPinNo,
+  });
+  return data;
+};

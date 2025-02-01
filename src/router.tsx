3e61cdc572500ad7bf6dbef6d93a1c1926ef2785
taewok <taewok5163@naver.com>
2025-02-01 16:29:19 +0900

    feat: 설정 page 기본 children 라우팅 설정

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,8 @@ import Created from './pages/mypage/components/Created';
 import Saved from './pages/mypage/components/savedTab/Saved';
 import Main from './pages/main/Main';
 import PinCreate from './pages/pinCreate/PinCreate';
+import Setting from './pages/setting/Setting';
+import ProfileEdit from './pages/profileEdit/ProfileEdit';
 
 export const router = createBrowserRouter([
   {
@@ -23,6 +25,11 @@ export const router = createBrowserRouter([
         ],
       },
       { path: 'pincreate', element: <PinCreate /> },
+      {
+        path: 'setting',
+        element: <Setting />,
+        children: [{ path: '', element: <ProfileEdit /> }],
+      },
     ],
   },
 ]);

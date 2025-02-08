63a46e73d39a7ccbe20ccfb43b1c621db742ab5a
taewok <taewok5163@naver.com>
2025-02-08 16:43:43 +0900

    feat: 캘린더 팝업 추가 & 캘린더 외 클릭시 close

--- src/pages/accountManagement/components/privacySetting/Birth.tsx
+++ src/pages/accountManagement/components/privacySetting/Birth.tsx
@@ -1,5 +1,10 @@
 import Input from '@/components/common/Input';
 import InfoIcon from '@/components/icons/InfoIcon';
+import Calendar from 'react-calendar';
+import './css/Calendar.css';
+import CalendarIcon from '@/components/icons/CalendarIcon';
+import { useRef, useState } from 'react';
+import useOutsideClick from '@/hooks/useOutsideClick';
 
 interface BirthProps {
   birth: string;
@@ -7,6 +12,23 @@ interface BirthProps {
 }
 
 const Birth = ({ birth, birthOnChange }: BirthProps) => {
+  // 달력 창 활성화 여부
+  const [isCalendar, setIsCalendar] = useState<boolean>(false);
+
+  /** 달력창 활성화 여부 toggle 함수 */
+  const isCalendarToggle = () => {
+    setIsCalendar(!isCalendar);
+  };
+
+  // 달력 요소 ref
+  const claendarRef = useRef<HTMLDivElement | null>(null);
+
+  // 달력 요소외의 클릭시 감지 후 isCalendarToggle 실행
+  useOutsideClick({
+    ref: claendarRef,
+    callback: () => isCalendarToggle(),
+  });
+
   return (
     <div>
       <div className="flex items-center mb-1">
@@ -25,13 +47,39 @@ const Birth = ({ birth, birthOnChange }: BirthProps) => {
           </div>
         </div>
       </div>
-      <Input
-        className="w-full"
-        onChangeFC={birthOnChange}
-        placeholder=""
-        value={birth}
-        id="birth-input"
-      />
+
+      <div className="relative" onClick={isCalendarToggle}>
+        <Input
+          className="w-full"
+          onChangeFC={birthOnChange}
+          placeholder=""
+          value={birth}
+          id="birth-input"
+        />
+
+        <span className="absolute top-1/2 -translate-y-1/2 right-5">
+          <CalendarIcon />
+        </span>
+
+        {isCalendar && (
+          <div
+            ref={claendarRef}
+            onClick={(e) => e.stopPropagation()}
+            className="absolute z-50 top-full translate-y-2 right-1/2 translate-x-1/2"
+          >
+            <Calendar
+              onChange={(e) => birthOnChange(String(e))}
+              value={birth}
+              next2Label={null}
+              prev2Label={null}
+              showNeighboringMonth={false}
+              calendarType={'hebrew'}
+              formatDay={(locale, date) => date.getDate().toString()}
+              className="relative w-[312px] rounded-2xl bg-white shadow-custom-modal p-4 font-Pretendard"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };

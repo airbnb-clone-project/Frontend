f9e949c4b8ff271dd5bb59ac09cf661fe5b95ad6
taewok <taewok5163@naver.com>
2025-02-08 16:41:53 +0900

    feat: 캘린더 icon 컴포넌트 생성

---
+++ src/components/icons/CalendarIcon.tsx
@@ -0,0 +1,15 @@
+const CalendarIcon = () => {
+  return (
+    <svg
+      aria-label="달력 열기"
+      height="16"
+      role="img"
+      viewBox="0 0 24 24"
+      width="16"
+    >
+      <path d="M11 11H6v5h5zm9-7h-2.5V2.5a1.5 1.5 0 1 0-3 0V4h-5V2.5a1.5 1.5 0 1 0-3 0V4H4a3 3 0 0 0-3 3v13a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3M4 9h16v11H4z"></path>
+    </svg>
+  );
+};
+
+export default CalendarIcon;

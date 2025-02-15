723e854d026add9943fc4a0919cd581ba8011b10
taewok <taewok5163@naver.com>
2025-02-15 10:16:36 +0900

    style: cursor-pointer & id prop 추가

--- src/components/common/LabelInput.tsx
+++ src/components/common/LabelInput.tsx
@@ -10,6 +10,7 @@ interface LabelInputProps {
   isEffect?: boolean;
   className?: string;
   inputClassName?: string;
+  id?: string;
 }
 const LabelInput = React.memo(
   ({
@@ -20,12 +21,16 @@ const LabelInput = React.memo(
     isEffect,
     className,
     inputClassName,
+    id,
     ...props // 기본 input 속성 전달
   }: LabelInputProps & React.InputHTMLAttributes<HTMLInputElement>) => {
     return (
       <div className={twMerge('flex flex-col', className)}>
-        <label className="text-xs mb-2">{title}</label>
+        <label className="text-xs mb-2 cursor-pointer" htmlFor={id}>
+          {title}
+        </label>
         <Input
+          id={id}
           onChangeFC={onChangeFC}
           placeholder={placeholder}
           value={value}
